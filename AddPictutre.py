6c905b1e31f68d8e3333363417fc2647e11a56b7
BogBogdan <bogdantomicbttomic@gmail.com>
2024-02-07 22:16:59 +0100

    konfigurisano sve

--- AddPictutre.py
+++ AddPictutre.py
@@ -19,3 +19,56 @@ def start():
     driver = Driver.getDriver()
     username, password = getCredentials.usernameNpassword()
     
+    driver.get("http://www.instagram.com")
+
+    # Pauza od 5 sekundi (prilagodite prema potrebi)
+    time.sleep(random.randrange(4,5))
+
+    AuthFunction.auth(driver, username, password)
+
+    time.sleep(random.randrange(4,5))
+
+    OpenNewPostFunction.sacekaj_i_klikni(driver)
+
+    time.sleep(random.randrange(4,5))
+
+    AddImage.izaberi_sliku(driver, "C:/Users/bogda/OneDrive/Desktop/Slike/2.jpg")
+    
+    time.sleep(random.randrange(4,5))
+
+    NotNowStopFunction.klikni_na_not_now(driver)
+
+    time.sleep(random.randrange(4,5))
+
+    OpenNewPostFunction.sacekaj_i_klikni(driver)
+
+    time.sleep(random.randrange(4,5))
+
+    AddImage.izaberi_sliku(driver, "C:/Users/bogda/OneDrive/Desktop/Slike/2.jpg")
+
+    time.sleep(random.randrange(4,5))
+
+    OriginalImageSize.optimization_of_image(driver)
+
+    time.sleep(random.randrange(4,5))
+
+    OriginalImageSize.select_original_optimization(driver)
+
+    time.sleep(random.randrange(4,5))
+    
+    ClickButtonNext.click_on_button_next(driver)
+
+    time.sleep(random.randrange(4,5))
+
+    ClickButtonNext.click_on_button_next(driver)
+
+    time.sleep(random.randrange(4,5))
+
+    AddTextToImage.insert_text_into_element(driver, "Ovo je neki tekst \n #freedom")
+
+    time.sleep(random.randrange(60,70))
+
+    # Zatvaranje pregledača
+    driver.quit()
+    
+start()
